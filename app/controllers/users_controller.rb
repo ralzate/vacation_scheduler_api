3f2c82733d749abc8ce691a2990f559ca90e1b7a
Alejandro Saldarriaga Alzate <alejoalzate12@gmail.com>
2024-06-26 06:19:54 -0500

    Add user email validation, UsersController#create action, and JWT service

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+    def create
+      user = User.new(user_params)
+  
+      if user.save
+        render json: { message: 'User created successfully' }, status: :created
+      else
+        render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+      end
+    end
+  
+    private
+  
+    def user_params
+      params.permit(:email, :password, :password_confirmation)
+    end
+  end
+  
