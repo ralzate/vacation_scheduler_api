039e483fcccf4cb5f7551098a31216cd8a13f9dc
Alejandro Saldarriaga Alzate <alejoalzate12@gmail.com>
2024-06-26 05:30:08 -0500

    Add test data for vacation model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+Vacation.destroy_all
+
+20.times do
+  Vacation.create(
+    employee_full_name: Faker::Name.name,
+    start_date: Faker::Date.between(from: 1.year.ago, to: Date.today),
+    end_date: Faker::Date.between(from: Date.today, to: 1.year.from_now)
+  )
+end
+
+puts "Seeds loaded successfully!"
