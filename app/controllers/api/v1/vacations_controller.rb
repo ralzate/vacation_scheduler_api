5e24960208e4f63250756e83392fe44790fcd410
Alejandro Saldarriaga Alzate <alejoalzate12@gmail.com>
2024-06-26 05:09:41 -0500

    Implemented CRUD operations for Vacation model in Api::V1::VacationsController

---
+++ app/controllers/api/v1/vacations_controller.rb
@@ -0,0 +1,45 @@
+class Api::V1::VacationsController < ApplicationController
+    before_action :set_vacation, only: [:show, :update, :destroy]
+  
+    def index
+      @vacations = Vacation.all
+      render json: @vacations
+    end
+  
+    def show
+      render json: @vacation
+    end
+  
+    def create
+      @vacation = Vacation.new(vacation_params)
+  
+      if @vacation.save
+        render json: @vacation, status: :created
+      else
+        render json: @vacation.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def update
+      if @vacation.update(vacation_params)
+        render json: @vacation
+      else
+        render json: @vacation.errors, status: :unprocessable_entity
+      end
+    end
+  
+    def destroy
+      @vacation.destroy
+      head :no_content
+    end
+  
+    private
+      def set_vacation
+        @vacation = Vacation.find(params[:id])
+      end
+  
+      def vacation_params
+        params.require(:vacation).permit(:employee_name, :start_date, :end_date)
+      end
+  end
+  
