984e501d3d5e31e3975c823b17849c0a4960323a
Alejandro Saldarriaga Alzate <alejoalzate12@gmail.com>
2024-06-27 22:10:58 -0500

    Better exception handling in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_request
-
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from JWT::DecodeError, with: :handle_jwt_decode_error
   private
 
   def authenticate_request
+  end
+
+  def record_not_found(exception)
+    render json: { error: 'Record not found' }, status: :not_found
+  end
+
+  def handle_jwt_decode_error(exception)
+    render json: { error: 'Invalid token' }, status: :unauthorized
   end
 end
